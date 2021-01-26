84ac2e0221210049be30f272e58c4e6cb8a5a10b
Krishna Mistry <kmistry217@gmail.com>
2021-01-26 18:22:18 -0500

    new ticket page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import SignUp from "./components/SignUp"
+import NewTicket from "./components/NewTicket"
 import './App.css';
 
 function App() {
   return (
     <div>
 		<SignUp></SignUp>
+		<NewTicket></NewTicket>
     </div>
   );
 }
---
+++ ui/src/components/NewTicket.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react" 
+
+const NewTicket = () => {
+
+    const [title, setTitle] = useState("");
+	const [price, setPrice] = useState("");
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		//BACKEND - post ticket request goes here
+		console.log(title, price);
+	}
+
+	const validateForm = () => {
+		return title.length>0 && price>0;
+	}
+
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1> Create a Ticket </h1>
+            <div className="form-group">
+                <label> Title: </label>
+                <input
+                    className="form-control"
+                    value={title} 
+          		    onChange={(e) => setTitle(e.target.value)}
+                />
+            </div>
+            <div className="form-group">
+                <label> Price: </label>
+                <input
+                    className="form-control"
+                    value={price} 
+          		    onChange={(e) => setPrice(e.target.value)}
+                />
+            </div>
+            <button className="btn btn-primary" disabled={!validateForm()}> Sign Up </button>
+        </form>
+    );
+};
+
+export default NewTicket;
