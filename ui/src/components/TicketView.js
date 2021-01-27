2c9b8ccbc89de3e15e4c53b46311a5f085789007
Neil <Neil@Enterprise.local>
2021-01-27 15:07:21 -0600

    changed ticketview, removed purchase button

--- ui/src/components/TicketView.js
+++ ui/src/components/TicketView.js
@@ -5,23 +5,10 @@ import Header from './Header';
 // import { useDispatch, useSelector } from 'react-redux'
 
 const TicketView = (ticket) => {
-    console.log(ticket.value)
-    const renderBuyBtn = () => {
-        if (ticket.value.available){
-            return (
-                <button>Purchase</button>
-            )
-        }else{
-            return (
-                <button>Unavailable</button>
-            )
-        }
-    }
     return (
         <div>
             <h1>{ticket.value.title}</h1>
             <p>{ticket.value.price}</p>
-            <p>{renderBuyBtn()}</p>
         </div>
     )
 }
