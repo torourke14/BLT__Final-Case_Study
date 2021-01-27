190e4548505693337431c3c1c7acfcb8243e5239
Neil <Neil@Enterprise.local>
2021-01-27 12:31:12 -0600

    Decided to stop being dumb and use setTimeout like a normal developer

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,13 +4,14 @@ import NewTicket from "./components/NewTicket"
 import TicketList from "./components/TicketList"
 import TicketPurchase from "./components/TicketPurchase"
 import TicketView from "./components/TicketView"
+import TicketTimer from "./components/TicketTimer"
 
 import './App.css';
 
 function App() {
   return (
     <div>
-        <TicketList/>
+        <TicketTimer />
     </div>
   );
 }
--- ui/src/components/TicketTimer.js
+++ ui/src/components/TicketTimer.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
-const  TicketTimer = (ticket) => {
+const  TicketTimer = (/*ticket*/) => {
 
     let [time, setTime] = useState(30);
-
+    const ticket = {title: 'bball', price: 40, userId: 'asdf', orderId: 'asdf', available: true}
     useEffect(() => {
-        const interval = setInterval(()=>{
-            setTime(time-1);
-        }, 1000);
+        time > 0 && setTimeout(() => setTime(time - 1), 1000);
     },[time])
+
+
     return (
         <div>
-            <h1> Purchasing {ticket.title}</h1>
+            <h1> Purchasing {ticket.title} Ticket</h1>
             <h2> You have {time} seconds left</h2>
         </div>
     );
