786a2bcc1224916bfe060acedd4c9aa1fade6a9f
Laura Antonescu <laantones@gmail.com>
2021-01-27 13:38:40 -0700

    Working on login screen

---
+++ native-ui/app/screens/LoginScreen.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Image, TextInput } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import {
+    AppForm as Form,
+    AppFormField as FormField,
+    SubmitButton,
+  } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+email: Yup.string().required().email().label("Email"),
+password: Yup.string().required().min(4).label("Password"),
+});
+  
+function LoginScreen() {
+    return (
+      <Screen style={styles.container}>
+        <Form
+          initialValues={{ name: "", email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+          //validationSchema={validationSchema}
+        >
+          <FormField
+            autoCapitalize="none"
+            autoCorrect={false}
+            icon="email"
+            keyboardType="email-address"
+            name="email"
+            placeholder="Email"
+            textContentType="emailAddress"
+          />
+          <FormField
+            autoCapitalize="none"
+            autoCorrect={false}
+            icon="lock"
+            name="password"
+            placeholder="Password"
+            secureTextEntry
+            textContentType="password"
+          />
+          <SubmitButton title="Login" />
+          <SubmitButton title="Register" />
+        </Form>
+      </Screen>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      padding: 10,
+    },
+  });
+  
+  export default LoginScreen;
