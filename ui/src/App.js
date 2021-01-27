76362b0080c13afc49e6e255659aba548bde010a
Krishna Mistry <kmistry217@gmail.com>
2021-01-27 14:58:25 -0500

    Router with Navbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import SignUp from "./components/SignUp"
+import SignIn from "./components/SignIn"
 import NewTicket from "./components/NewTicket"
 import TicketList from "./components/TicketList"
 import TicketPurchase from "./components/TicketPurchase"
@@ -7,11 +8,25 @@ import TicketView from "./components/TicketView"
 
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div>
-        <TicketList/>
-    </div>
+    <Router>
+      <div>
+          <Header/>
+          <Switch>
+            <Route path="/" component={TicketList} exact/>
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/signin" component={SignIn}/>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
