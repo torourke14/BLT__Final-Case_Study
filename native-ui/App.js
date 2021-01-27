8d77806209b37dfa816e13f53c752b6ead72bfc4
Laura Antonescu <laantones@gmail.com>
2021-01-27 13:38:40 -0700

    Working on login screen

--- native-ui/App.js
+++ native-ui/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TicketListingScreen from './app/screens/TicketListingScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TicketListingScreen />
+      <LoginScreen />
     </View>
   );
 }
