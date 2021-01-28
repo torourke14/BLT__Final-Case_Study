72812c039fc09bf4007ff52ba4afdfd67cbfb4ba
Rajene Harris <45857126+aquaomarine@users.noreply.github.com>
2021-01-28 15:53:32 -0500

    update

--- api/payments/events/listeners/order-created-listener.js
+++ api/payments/events/listeners/order-created-listener.js
@@ -1,20 +1,5 @@
-import { Message } from 'node-nats-streaming';
-var events = require ('events');
+// import { Message } from 'node-nats-streaming';
+// var events = require ('events');
 
-//eventEmitter object
-var eventEmitter = new events.EventEmitter()
+// const { getEventListeners, EventEmitter } = require('OrderCreatedEvent');
 
-const { getEventListeners, EventEmitter } = require('OrderCreatedEvent');
-
-{
-  const ee = new EventEmitter();
-  const listener = () => console.log('Events are fun');
-  ee.on('OrderCreatedEvent', listener);
-  getEventListeners(ee, OrderCreatedEvent); // [listener]
-}
-{
-  const et = new EventTarget();
-  const listener = () => console.log('Events are fun');
-  et.addEventListener('OrderCreatedEvent', listener);
-  getEventListeners(et, 'OrderCreatedEvent'); // [listener]
-}
