2f26b3f8f5f427b8597180a149c1f2c18d58d8fd
Laura Antonescu <laantones@gmail.com>
2021-01-27 21:45:52 -0700

    Added styling to Ticket

--- native-ui/app/components/Ticket.js
+++ native-ui/app/components/Ticket.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import colors from "../config/colors"
 
 /**
  * Ticket
@@ -10,7 +11,8 @@ const Ticket = ({ticket}) => {
     
     return (
         <View style={styles.listing}>
-            <Text style={styles.text}>{ticket.title} - ${ticket.price}</Text>
+            <Text style={styles.price}>${ticket.price}</Text>
+            <Text style={styles.text}>{ticket.title}</Text>
         </View>
     )
 }
@@ -19,10 +21,28 @@ const styles = StyleSheet.create({
     listing: {
         flex: 1,
         flexDirection: 'column',
+        borderStyle: 'solid',
+        borderColor: 'black',
+        backgroundColor: colors.white,
+        borderWidth: 1,
+        marginTop: 5,
         padding: 10,
     },
-    text: {
+    price: {
         flex: 1,
+        width: 40,
+        flexDirection: 'column',
+        borderStyle: 'solid',
+        borderColor: 'transparent',
+        backgroundColor: 'yellow',
+        borderWidth: 1,
+        fontSize: 16,
+    },
+    text: {
+        backgroundColor: colors.light,
+        flex: 1,
+        fontSize: 18,
+        margin: 10,
     }
 })
 
