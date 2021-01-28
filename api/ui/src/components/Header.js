60f35e31039438d0da502397dffa119e916667db
torourke14 <tannero@live.com>
2021-01-28 12:17:15 -0800

    moved ui into services folder

---
+++ api/ui/src/components/Header.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react" 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+
+    //have to see if user is sign in or out to choose which header to display
+    let {user} = props;
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">GitTix</Link>
+            {user ? 
+                <div className="navbar-nav">
+                    <Link className="nav-item nav-link active" to="/myorders"> My Orders </Link>
+                    <Link className="nav-item nav-link active" to="tickets/new"> Sell Tickets</Link>
+                    <Link className="nav-item nav-link active" to="/signout"> Sign Out</Link>          
+                </div> : 
+                <div className="navbar-nav">
+                    <Link className="nav-item nav-link active" to="/signup"> Sign Up </Link>
+                    <Link className="nav-item nav-link active" to="signin"> Sign In </Link>         
+                </div>
+            }
+            
+            
+            
+        </nav>
+    );
+};
+
+export default Header;
