e93f10a27e85e3a37408d63f124c4141c263c52b
clay13chopper <clay13ch@vt.edu>
2021-01-27 20:16:14 -0500

    Basic ticket api calls set up

--- api/tickets/index.js
+++ api/tickets/index.js
@@ -8,61 +8,86 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const commentsByPostId = {};
+const tickets = {};
 
-app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
+app.get('/api/tickets', (req, res) => {
+  res.send(tickets || []);
 });
 
-app.post('/posts/:id/comments', async (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
-  const { content } = req.body;
-
-  const comments = commentsByPostId[req.params.id] || [];
-
-  comments.push({ id: commentId, content, status: 'pending' });
-
-  commentsByPostId[req.params.id] = comments;
-
-  await axios.post('http://event-bus-clusterip-svc:4005/events', {
-    type: 'CommentCreated',
-    data: {
-      id: commentId,
-      content,
-      postId: req.params.id,
-      status: 'pending'
-    }
-  });
-
-  res.status(201).send(comments);
+app.get('/api/tickets/:id', (req, res) => {
+  res.send(tickets[req.params.id] || []);
 });
 
-app.post('/events', async (req, res) => {
-  console.log('Event Received:', req.body.type);
-  const { type, data } = req.body;
-  console.log('Received Event', req.body.type);
-  if (type === 'CommentModerated') {
-    const { postId, id, status, content } = data;
-    const comments = commentsByPostId[postId];
+app.post('/api/tickets', async (req, res) => { //posts/:id/comments
+  const ticketId = randomBytes(4).toString('hex');
+  const { title, price } = req.body;
 
-    const comment = comments.find(comment => {
-      return comment.id === id;
-    });
-    comment.status = status;
+  tickets[ticketId] = {
+    ticketId,
+    title,
+    price
+  };
 
-    await axios.post('http://event-bus-clusterip-svc:4005/events', {
-      type: 'CommentUpdated',
-      data: {
-        id,
-        status,
-        postId,
-        content
-      }
-    });
-  }
+  // await axios.post('http://event-bus-clusterip-svc:4005/events', {
+  //   type: 'TicketCreated',
+  //   data: {
+  //     id: ticketId,
+  //     title,
+  //     price
+  //   }
+  // });
 
-  res.send({});
+  res.status(201).send(tickets[ticketId]);
 });
+
+app.put('/api/tickets/:id', async (req, res) => { //posts/:id/comments
+  const ticketId = req.params.id;
+  const { title, price } = req.body;
+
+  tickets[ticketId] = {
+    ticketId,
+    title,
+    price
+  };
+
+  // await axios.post('http://event-bus-clusterip-svc:4005/events', {
+  //   type: 'TicketUpdated',
+  //   data: {
+  //     id: ticketId,
+  //     title,
+  //     price
+  //   }
+  // });
+
+  res.status(201).send(tickets[ticketId]);
+});
+
+// app.post('/events', async (req, res) => {
+//   console.log('Event Received:', req.body.type);
+//   const { type, data } = req.body;
+//   console.log('Received Event', req.body.type);
+//   if (type === 'CommentModerated') {
+//     const { postId, id, status, content } = data;
+//     const comments = tickets[postId];
+
+//     const comment = comments.find(comment => {
+//       return comment.id === id;
+//     });
+//     comment.status = status;
+
+//     await axios.post('http://event-bus-clusterip-svc:4005/events', {
+//       type: 'TicketUpdated',
+//       data: {
+//         id,
+//         status,
+//         postId,
+//         content
+//       }
+//     });
+//   }
+
+//   res.send({});
+// });
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
