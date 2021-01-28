0544ca9bd6822f1a60856ece3a16f2b523b6a921
clay13chopper <clay13ch@vt.edu>
2021-01-28 14:29:12 -0500

    Fixed indent to be 4 spaces, implemented database storage

--- api/tickets/index.js
+++ api/tickets/index.js
@@ -1,8 +1,8 @@
 /*
-index.js
-@Author Clayton Cunningham
-
-Ticket record program for StubHub clone.
+ *  index.js
+ *  @Author Clayton Cunningham
+ *
+ *  Ticket record program for StubHub clone.
 */
 
 const express = require('express');
@@ -11,184 +11,185 @@ const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 const { body, validationResult } = require('express-validator');
-
-const MongoClient = require('mongodb').MongoClient;
+const { Ticket } = require('./models/ticket.js');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const tickets = {};
-
 const uri = "mongodb+srv://admin:12345@cluster0.l6u68.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-// async function main() {
-//   const client = new MongoClient(uri, { useNewUrlParser: true });
-//   // client.connect(err => {
-//   //   // const collection = client.db("test").collection("devices");
-//   //   const collection = client.db().admin().listDatabases();
-//   //   // perform actions on the collection object
-//   //   // collection.databases.forEach(db => {
-//   //   //   console.log(' - ${db.name}')
-//   //   // });
-//   //   console.log(collection)
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 
-//   //   client.close();
-//   // });
-//   try {
-//     await client.connect();
-//     collection = await client.db().admin().listDatabases(); 
-//     collection.databases.forEach(db => {
-//       console.log(' - '+ db.name)
-//     });
-//     console.log(collection);
-//   }
-//   catch(e) {
-//     console.error(e)
-//   }
+// function errorHandler(err, req, res, next) {
+//   console.log('ERROR! - Something went wrong', err)
+
+//   res.status(400).send({
+//       message: `ERROR! - ${err.message}`
+//   })
 // }
-
-function errorHandler(err, req, res, next) {
-  console.log('ERROR! - Something went wrong', err)
-
-  res.status(400).send({
-      message: `ERROR! - ${err.message}`
-  })
-}
 
 app.get('/api/tickets', (req, res) => {
   // main();
-  res.send(tickets || []);
+
+    Ticket.find({}, (err, tickets) => {
+        if (err) throw err
+        console.log("Here are the tickets")
+        console.log(tickets)
+        res.send(tickets || [])
+    })
 });
+
 
 app.get('/api/tickets/:id', (req, res) => {
-  res.send(tickets[req.params.id] || []);
+
+    Ticket.findById(req.params.id, (err, ticket) => {
+        if (err) throw err
+        console.log("Here is the ticket")
+        console.log(ticket)
+        res.send(ticket || [])
+    })
+
 });
+
 
 app.post('/api/tickets', [
-  body('title').not().isEmpty().withMessage('Title is required'),
-  body('price')
-    .isFloat({ gt: 0 })
-    .withMessage('Price must be greater than 0'),
-],
-async (req, res) => {
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+        .isFloat({ gt: 0 })
+        .withMessage('Price must be greater than 0'),
+    ],
+    async (req, res) => {
 
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  //Send errors to user
-  const error = new Error('Invalid title or price!');
-  error.descriptors = errors.array();
+    //Send errors to user
+    const error = new Error('Invalid title or price!');
+    error.descriptors = errors.array();
 
-  if(!errors.isEmpty()){
-      error.descriptors = errors.array();
-      res.status(201).send(error.descriptors[0].msg);
-      return;
-  }
-
-  const ticketId = randomBytes(4).toString('hex');
-  const { title, price } = req.body;
-
-  tickets[ticketId] = {
-    ticketId,
-    title,
-    price
-  };
-
-  await axios.post('http://event-bus-clusterip-svc:4005/events', {
-    type: 'TicketCreated',
-    data: {
-      id: ticketId,
-      title,
-      price
+    if(!errors.isEmpty()){
+        error.descriptors = errors.array();
+        res.status(201).send(error.descriptors[0].msg);
+        return;
     }
-  });
 
-  res.status(201).send(tickets[ticketId]);
+    const { title, price } = req.body;
+
+    const ticket = new Ticket({
+        title,
+        price
+    });
+    await ticket.save();
+
+    await axios.post('http://event-bus-clusterip-svc:4005/events', {
+        type: 'TicketCreated',
+        data: {
+        ticketId,
+        title,
+        price
+        }
+    });
+
+    res.status(201).send(ticket);
 });
 
+
 app.put('/api/tickets/:id', [
-  body('title').not().isEmpty().withMessage('Title is required'),
-  body('price')
-    .isFloat({ gt: 0 })
-    .withMessage('Price must be greater than 0'),
-],
-async (req, res) => { 
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+        .isFloat({ gt: 0 })
+        .withMessage('Price must be greater than 0'),
+    ],
+    async (req, res) => { 
 
-  const errors = validationResult(req);
-
-  //Send errors to user
-  const error = new Error('Invalid title or price!');
-  error.descriptors = errors.array();
-
-  if(!errors.isEmpty()){
-      error.descriptors = errors.array();
-      res.status(201).send(error.descriptors[0].msg);
-      return;
-  }
-
-  const ticketId = req.params.id;
-  if (tickets[ticketId] == undefined) {
-    res.status(201).send('No valid ticket found');
-    return;
-  }
-  if (tickets[ticketId].orderId != undefined) res.status(201).send('Cannot edit a reserved ticket'); 
-
-  const { title, price } = req.body;
-
-  tickets[ticketId] = {
-    ticketId,
-    title,
-    price
-  };
-
-  await axios.post('http://event-bus-clusterip-svc:4005/events', {
-    type: 'TicketUpdated',
-    data: {
-      id: ticketId,
-      title,
-      price
+    const errors = validationResult(req);
+    //Send errors to user
+    const error = new Error('Invalid title or price!');
+    error.descriptors = errors.array();
+    if(!errors.isEmpty()){
+        error.descriptors = errors.array();
+        res.status(201).send(error.descriptors[0].msg);
+        return;
     }
-  });
 
-  res.status(201).send(tickets[ticketId]);
+    const ticketId = req.params.id;
+    const ticket = await Ticket.findById(req.params.id);
+    if (ticket == undefined) {
+        res.status(201).send('No valid ticket found');
+        return;
+    }
+    if (ticket.orderId != undefined) {
+        res.status(201).send('Cannot edit a reserved ticket'); 
+        return;
+    }
+
+    const { title, price } = req.body;
+    ticket.set({
+        title,
+        price,
+    });
+    await ticket.save();
+    console.log(ticket)
+
+    await axios.post('http://event-bus-clusterip-svc:4005/events', {
+        type: 'TicketUpdated',
+        data: {
+        ticketId,
+        title,
+        price
+        }
+    });
+
+    res.status(201).send(ticket);
 });
 
 app.post('/events', async (req, res) => {
-  const { type, data } = req.body;
-  console.log('Received Event', req.body.type);
-  if (type === 'OrderCreated') {
-    const { ticketId, orderId } = data;
+    const { type, data } = req.body;
+    console.log('Received Event', req.body.type);
+    if (type === 'OrderCreated') {
+        const { ticketId, orderId } = data;
 
-    tickets[ticketId].orderId = orderId;
+        const ticket = await Ticket.findById(ticketId);
+        ticket.set({
+            orderId
+        });
+        await ticket.save();
 
-    await axios.post('http://event-bus-clusterip-svc:4005/events', {
-      type: 'TicketUpdated',
-      data: {
-        ticketId, 
-        title: tickets[ticketId].title,
-        price: tickets[ticketId].price,
-        orderId
-      }
-    });
-  }
-  else if (type === 'OrderCancelled') {
-    const { ticketId } = data;
+        await axios.post('http://event-bus-clusterip-svc:4005/events', {
+        type: 'TicketUpdated',
+        data: {
+            ticketId, 
+            title: ticket.title,
+            price: ticket.price,
+            orderId
+        }
+        });
+    }
+    else if (type === 'OrderCancelled') {
+        const { ticketId } = data;
+        
+        const ticket = await Ticket.findById(ticketId);
+        ticket.set({
+            orderId: undefined
+        });
+        await ticket.save();
 
-    delete tickets[ticketId].orderId;
+        await axios.post('http://event-bus-clusterip-svc:4005/events', {
+        type: 'TicketUpdated',
+        data: {
+            ticketId, 
+            title: ticket.title,
+            price: ticket.price
+        }
+        });
+    }
 
-    await axios.post('http://event-bus-clusterip-svc:4005/events', {
-      type: 'TicketUpdated',
-      data: {
-        ticketId, 
-        title: tickets[ticketId].title,
-        price: tickets[ticketId].price
-      }
-    });
-  }
-
-  res.send({});
+    res.send({});
 });
 
 app.listen(4001, () => {
-  console.log('Listening on 4001');
+    console.log('Listening on 4001');
 });
+
+
